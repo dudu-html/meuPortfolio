d48b0d29518167a7e19dc65c4ec767849c861945
Eduardo Moreira Lauermann <eduardomoreiralau@gmail.com>
2023-07-31 14:21:47 -0300

    (Desenvolvimento) Segundo commit - infos

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ let buttonCopy = document.querySelector('#copy-button');
 
 buttonCopy.addEventListener("click", function copiarTexto() {
     let textoCopiar = document.getElementById("email").textContent;
+    // Retorno:
     console.log('Email Copiado com sucesso');
+    window.alert('copia');
     navigator.clipboard.writeText(textoCopiar);
 });
 
