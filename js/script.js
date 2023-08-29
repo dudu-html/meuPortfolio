1c35cd53c4871b37de3ce46265e9b39c5cfa3c95
Eduardo Moreira Lauermann <eduardomoreiralau@gmail.com>
2023-08-29 12:01:18 -0300

    Atualização email

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ buttonCopy.addEventListener("click", function copiarTexto() {
     let textoCopiar = document.getElementById("email").textContent;
     // Retorno:
     console.log('Email Copiado com sucesso');
-    window.alert('copia');
+    window.alert('Email copiado!');
     navigator.clipboard.writeText(textoCopiar);
 });
 
